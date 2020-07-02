4726c62d44fee382c2e9bf3b1cf83f69576d9878
Donald Silveira <donaldsilveira@gmail.com>
2020-07-02 15:13:19 -0300

    feat(pages): Login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,67 @@
 import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
+
+import Title from '../../components/title';
+import FromControl from '../../components/form-control';
+import FieldLabel from '../../components/field-label';
+import FieldError from '../../components/field-error';
+import TextField from '../../components/text-field';
+import Button from '../../components/button';
 
 import { LoginProps } from './props';
-import { Container } from './styles';
+import { Container, TitleContainer, Form } from './styles';
 
-const Login:React.FC<LoginProps> = () => (
-  <Container />
-);
+type Inputs = {
+  email: string,
+  password: string,
+};
+
+const Login:React.FC<LoginProps> = () => {
+  const history = useHistory();
+  const { control, handleSubmit, errors } = useForm<Inputs>();
+
+  const handleOnSubmit = (data: { email: string, password: string }) => {
+    history.replace('/');
+  };
+
+  return (
+  <Container>
+    <TitleContainer>
+      <Title>Agenda de Churrascos</Title>
+    </TitleContainer>
+
+    <Form onSubmit={handleSubmit(handleOnSubmit)}>
+      <FromControl>
+        <FieldLabel>Login</FieldLabel>
+        <Controller
+          as={TextField}
+          name="email"
+          placeholder="E-mail"
+          control={control}
+          defaultValue="churras@trin.ca"
+          rules={{ required: { value: true, message: 'O campo e-mail é obrigatório' } }}
+        />
+        {errors.email && <FieldError>{errors.email?.message}</FieldError>}
+      </FromControl>
+
+      <FromControl>
+        <FieldLabel>Senha</FieldLabel>
+        <Controller
+          type="password"
+          as={TextField}
+          name="password"
+          placeholder="Senha"
+          control={control}
+          defaultValue="123456789"
+          rules={{ required: { value: true, message: 'O campo senha é obrigatório' } }}
+        />
+        {errors.password && <FieldError>{errors.password?.message}</FieldError>}
+      </FromControl>
+
+      <Button type="submit">Entrar</Button>
+    </Form>
+  </Container>
+);}
 
 export default Login;
--- src/pages/login/styles.tsx
+++ src/pages/login/styles.tsx
@@ -1,3 +1,31 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 100%;
+  background-image:
+    linear-gradient(0deg, ${({ theme }) => theme.colors.primary} 60%, rgba(255,255,255,0) 100%),
+    url(${require('../../assets/bg-pattern.svg')});
+  background-position: center;
+`;
+
+export const TitleContainer = styled.div`
+  padding: 70px 0 100px 0;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 0px 40px;
+
+  @media only screen and (min-width: 768px) {
+    padding: 0px 120px;
+  }
+
+  @media only screen and (min-width: 900px) {
+    padding: 0px 30%;
+  }
+
+  @media only screen and (min-width: 1600px) {
+    padding: 0px 40%;
+  }
+`;
